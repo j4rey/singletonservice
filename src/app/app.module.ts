7d933ef2c542491fbeea225fb9475469d9bf38d7
j4rey <j4rey@hotmail.com>
2018-04-22 11:14:19 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoreComponent } from './core/core.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoreComponent
   ],
   imports: [
     BrowserModule
